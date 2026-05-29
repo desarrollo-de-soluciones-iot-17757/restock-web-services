3e07b2b5743b85c3e34dacc04de864428cf79d98
Nicolas Juarez <juarezleonn2000@gmail.com>
2026-05-28 22:56:52 -0500

    feat(custom-supplies): add queries.

--- src/main/java/com/uitopic/restock/platform/resources/domain/model/queries/GetCustomSuppliesByAccountIdQuery.java
+++ src/main/java/com/uitopic/restock/platform/resources/domain/model/queries/GetCustomSuppliesByAccountIdQuery.java
@@ -1,4 +1,12 @@
 package com.uitopic.restock.platform.resources.domain.model.queries;
 
-public record GetCustomSuppliesByAccountIdQuery() {
+import com.uitopic.restock.platform.shared.domain.model.valueobjects.AccountId;
+
+/**
+ * Query to get all the custom supplies from a specific account by its ID.
+ *
+ * @param accountId the ID of the account to retrieve custom supplies for
+ */
+public record GetCustomSuppliesByAccountIdQuery(AccountId accountId) {
+
 }
