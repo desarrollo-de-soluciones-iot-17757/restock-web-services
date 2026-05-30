a788f60591091882d2ba1e8d0aa495cba5847345
Nicolas Juarez <juarezleonn2000@gmail.com>
2026-05-30 15:37:51 -0500

    fix: add unit measurement field for stock value objects.

--- src/main/java/com/uitopic/restock/platform/resources/domain/model/valueobjects/Stock.java
+++ src/main/java/com/uitopic/restock/platform/resources/domain/model/valueobjects/Stock.java
@@ -1,18 +1,27 @@
 package com.uitopic.restock.platform.resources.domain.model.valueobjects;
 
+import com.uitopic.restock.platform.resources.domain.exception.InvalidStockException;
 import com.uitopic.restock.platform.resources.domain.exception.InvalidStockQuantityException;
 
 /**
  * Value object representing the stock quantity of a product in the inventory. It encapsulates the logic for validating and manipulating stock quantities, ensuring that they are always non-negative and providing methods for adding and subtracting stock.
  *
  * @param stock the quantity of stock, which must be a non-negative integer
+ * @param unitMeasurement the unit of measurement for the stock quantity (e.g., "units", "kg", "liters", etc.)
  */
-public record Stock(Integer stock) {
+public record Stock(
+        Double stock,
+        String unitMeasurement
+) {
 
     // Constructor to validate the stock quantity
     public Stock {
         if (stock < 0) {
             throw new InvalidStockQuantityException("Stock quantity cannot be negative");
+        }
+
+        if (unitMeasurement.trim().isBlank()) {
+            throw new IllegalArgumentException("Unit measurement cannot be blank");
         }
     }
 
@@ -23,7 +32,11 @@ public record Stock(Integer stock) {
      * @return a new Stock instance with the resulting stock quantity after addition
      */
     public Stock add(Stock other) {
-        return new Stock(this.stock + other.stock);
+        if (!other.unitMeasurement().equals(this.unitMeasurement)) {
+            throw new InvalidStockException("Unit measurement does not match");
+        }
+
+        return new Stock(this.stock + other.stock(), this.unitMeasurement);
     }
 
     /**
@@ -37,14 +50,27 @@ public record Stock(Integer stock) {
         if (other.stock > this.stock) {
             throw new InvalidStockQuantityException("Cannot subtract more stock than available");
         }
-        return new Stock(this.stock - other.stock);
+
+        if (!other.unitMeasurement().equals(this.unitMeasurement)) {
+            throw new InvalidStockException("Unit measurement does not match");
+        }
+
+        return new Stock(this.stock - other.stock(), this.unitMeasurement);
     }
 
     /**
      * Get the current stock quantity.
      * @return the current stock quantity as an Integer.
      */
-    public Integer getValue() {
+    public Double getValue() {
         return this.stock;
+    }
+
+    /**
+     * Get the unit of measurement for the stock quantity.
+     * @return the unit of measurement as a String.
+     */
+    public String getUnitMeasurement() {
+        return this.unitMeasurement;
     }
 }
