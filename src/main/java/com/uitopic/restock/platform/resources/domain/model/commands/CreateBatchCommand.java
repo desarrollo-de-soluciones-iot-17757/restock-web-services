cf4721f4edf41f0180f263de0dc8818f81722605
Nicolas Juarez <juarezleonn2000@gmail.com>
2026-05-31 00:44:27 -0500

    feat(batches): add commands and queries.

--- src/main/java/com/uitopic/restock/platform/resources/domain/model/commands/CreateBatchCommand.java
+++ src/main/java/com/uitopic/restock/platform/resources/domain/model/commands/CreateBatchCommand.java
@@ -7,6 +7,19 @@ import com.uitopic.restock.platform.shared.domain.model.valueobjects.Money;
 import java.time.LocalDate;
 import java.util.Optional;
 
+/**
+ * Command to create a new batch of supplies in the inventory system.
+ *
+ * @param code the unique code for the batch
+ * @param initialStock the initial stock quantity for the batch
+ * @param unitPurchaseCost the cost per unit for purchasing the supplies in the batch
+ * @param customSupplyId the custom identifier for the supply associated with the batch
+ * @param receivingBranchId the identifier for the branch receiving the batch
+ * @param accountId the identifier for the account responsible for the batch
+ * @param manufacturingDate the optional manufacturing date of the supplies in the batch
+ * @param expirationDate the optional expiration date of the supplies in the batch
+ * @param entryDate the optional date when the batch was entered into the system
+ */
 public record CreateBatchCommand(
         String code,
         Stock initialStock,
--- src/main/java/com/uitopic/restock/platform/resources/domain/model/queries/GetBranchesByAccountIdQuery.java
+++ src/main/java/com/uitopic/restock/platform/resources/domain/model/queries/GetBranchesByAccountIdQuery.java
@@ -2,4 +2,8 @@ package com.uitopic.restock.platform.resources.domain.model.queries;
 
 import com.uitopic.restock.platform.shared.domain.model.valueobjects.AccountId;
 
+/**
+ * Query object for retrieving branches associated with a specific account ID.
+ * @param accountId The ID of the account for which to retrieve branches.
+ */
 public record GetBranchesByAccountIdQuery(AccountId accountId) {}
