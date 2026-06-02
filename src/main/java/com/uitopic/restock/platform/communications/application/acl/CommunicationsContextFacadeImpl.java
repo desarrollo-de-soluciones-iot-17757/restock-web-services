5fc6e2a3cc81c8a4b8eea9f65ad34d9eb96431b6
Nicolas Juarez <juarezleonn2000@gmail.com>
2026-06-01 19:43:08 -0500

    feat(communications): add unique entry point in the acl for generating notifications in the context.

--- src/main/java/com/uitopic/restock/platform/communications/application/acl/CommunicationsContextFacadeImpl.java
+++ src/main/java/com/uitopic/restock/platform/communications/application/acl/CommunicationsContextFacadeImpl.java
@@ -1,4 +1,20 @@
 package com.uitopic.restock.platform.communications.application.acl;
 
-public class CommunicationsContextFacadeImpl {
+import com.uitopic.restock.platform.communications.interfaces.acl.CommunicationsContextFacade;
+import com.uitopic.restock.platform.shared.domain.model.valueobjects.EmailContents;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommunicationsContextFacadeImpl implements CommunicationsContextFacade {
+
+    /**
+     * Creates a notification based on the provided email contents. This method is responsible for generating a notification that can be sent to users or other systems, based on the information contained in the EmailContents object.
+     *
+     * @param contents The contents of the email, including subject, body, recipient information, and any other relevant details needed to create a notification.
+     * @return A string representing the result of the notification creation process, which could be a success message, an identifier for the created notification, or any relevant information about the outcome of the operation.
+     */
+    @Override
+    public String createNotification(EmailContents contents) {
+        return "";
+    }
 }
