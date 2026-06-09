f5450fda889fee21a8f6c80882283f823a63da3c
Nicolas Juarez <juarezleonn2000@gmail.com>
2026-06-09 18:48:39 -0500

    feat(discrepancy-threshold): add method for retrieving a device threshold by device id.

--- src/main/java/com/uitopic/restock/platform/devices/domain/repositories/DeviceThresholdRepository.java
+++ src/main/java/com/uitopic/restock/platform/devices/domain/repositories/DeviceThresholdRepository.java
@@ -2,17 +2,52 @@ package com.uitopic.restock.platform.devices.domain.repositories;
 
 import com.uitopic.restock.platform.devices.domain.model.entities.DeviceThreshold;
 import com.uitopic.restock.platform.shared.domain.model.valueobjects.AccountId;
+import com.uitopic.restock.platform.tracking.domain.model.valueobjects.DeviceId;
 
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Repository interface for managing DeviceThreshold entities.
+ */
 public interface DeviceThresholdRepository {
 
+    /**
+     * Saves a DeviceThreshold entity to the repository.
+     *
+     * @param threshold The DeviceThreshold entity to be saved.
+     * @return The saved DeviceThreshold entity.
+     */
     DeviceThreshold save(DeviceThreshold threshold);
 
+    /**
+     * Finds a DeviceThreshold entity by its unique identifier.
+     *
+     * @param id The unique identifier of the DeviceThreshold entity.
+     * @return An Optional containing the found DeviceThreshold entity, or empty if not found.
+     */
     Optional<DeviceThreshold> findById(String id);
 
+    /**
+     * Finds a DeviceThreshold entity by the associated DeviceId.
+     *
+     * @param deviceId The DeviceId associated with the DeviceThreshold entity.
+     * @return An Optional containing the found DeviceThreshold entity, or empty if not found.
+     */
+    Optional<DeviceThreshold> findByDeviceId(DeviceId deviceId);
+
+    /**
+     * Finds all DeviceThreshold entities associated with a specific AccountId.
+     *
+     * @param accountId The AccountId for which to find the DeviceThreshold entities.
+     * @return A list of DeviceThreshold entities associated with the given AccountId.
+     */
     List<DeviceThreshold> findAllByAccountId(AccountId accountId);
 
+    /**
+     * Deletes a DeviceThreshold entity by its unique identifier.
+     *
+     * @param id The unique identifier of the DeviceThreshold entity to be deleted.
+     */
     void deleteById(String id);
 }
--- src/main/java/com/uitopic/restock/platform/devices/infrastructure/persistence/mongodb/repositories/DeviceThresholdPersistenceRepository.java
+++ src/main/java/com/uitopic/restock/platform/devices/infrastructure/persistence/mongodb/repositories/DeviceThresholdPersistenceRepository.java
@@ -2,13 +2,32 @@ package com.uitopic.restock.platform.devices.infrastructure.persistence.mongodb.
 
 import com.uitopic.restock.platform.devices.infrastructure.persistence.mongodb.entities.DeviceThresholdPersistenceEntity;
 import com.uitopic.restock.platform.shared.domain.model.valueobjects.AccountId;
+import com.uitopic.restock.platform.tracking.domain.model.valueobjects.DeviceId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+/**
+ * Repository interface for managing DeviceThresholdPersistenceEntity in MongoDB.
+ */
 @Repository
 public interface DeviceThresholdPersistenceRepository extends MongoRepository<DeviceThresholdPersistenceEntity, String> {
 
+    /**
+     * Find all DeviceThresholdPersistenceEntity by accountId.
+     *
+     * @param accountId the account ID to filter by
+     * @return a list of DeviceThresholdPersistenceEntity associated with the given account ID
+     */
     List<DeviceThresholdPersistenceEntity> findAllByAccountId(AccountId accountId);
+
+    /**
+     * Find a DeviceThresholdPersistenceEntity by deviceId.
+     *
+     * @param deviceId the device ID to filter by
+     * @return an Optional containing the DeviceThresholdPersistenceEntity if found, or empty if not found
+     */
+    Optional<DeviceThresholdPersistenceEntity> findByDeviceId(DeviceId deviceId);
 }
